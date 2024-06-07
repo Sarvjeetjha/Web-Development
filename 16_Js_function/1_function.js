184efcfae2c3635eae83c95492eb86fc53cea34a
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-07 21:52:03 +0530

    function expression

---
+++ 16_Js_function/1_function.js
@@ -0,0 +1,22 @@
+//function
+//function to greet
+
+
+// function great(){
+//     console.log("Hello Dear!! Good Morning");
+// }
+// great();
+
+//to add two number
+// function add(a,b){
+//     return (a+b);
+// }
+// console.log(add(25,300));
+
+//****************Function Expression */
+let variable=function(a,b){ //storing funstion referece in a variable
+    return a+b;
+}
+console.log(variable(2,50));
+
+
