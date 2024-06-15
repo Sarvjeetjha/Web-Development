683eb4dc9bdfe31132cba5ce33a1da740b2cad8a
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-15 22:29:45 +0530

    updated

---
+++ 16_Js_function/4_functionWithArgument.js
@@ -0,0 +1,15 @@
+//Function with argument
+// function Greet(name){
+//     console.log(` Hello ${name}`);
+
+// }
+// Greet("Ram");
+
+
+function Greet(name,age){
+    console.log(` Hello ${name} your age is ${age}`);
+
+}
+Greet("Ram",25);
+Greet(14);
+Greet("Ram");
