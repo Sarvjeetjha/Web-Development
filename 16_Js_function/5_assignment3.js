f8df67b35d80de7949c88a61b37dc15a6fd03d30
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-15 22:35:11 +0530

    function to display average of  three number

---
+++ 16_Js_function/5_assignment3.js
@@ -0,0 +1,7 @@
+//function to display average of three number
+function averageOfThreeNumber(num1,num2,num3){
+    const avr=(num1+num2+num3)/3;
+    console.log(avr)
+
+}
+averageOfThreeNumber(1,2,3)
