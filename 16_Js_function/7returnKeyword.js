94d08140c3921d79681848d75893cdb51744a48a
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-16 20:21:28 +0530

    return keyword

---
+++ 16_Js_function/7returnKeyword.js
@@ -0,0 +1,9 @@
+//used to return value from function
+//function to return sum of number
+
+function addNumber(num1,num2){
+    const sumNumber=num1+num2
+    return sumNumber
+}
+let ans= addNumber(2,6)
+console.log(ans);
