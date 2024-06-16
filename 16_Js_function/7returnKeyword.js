7d2da06d809c1b504bf565fc7764196afb8bf2bf
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-16 20:26:43 +0530

    calling nested function

--- 16_Js_function/7returnKeyword.js
+++ 16_Js_function/7returnKeyword.js
@@ -5,5 +5,9 @@ function addNumber(num1,num2){
     const sumNumber=num1+num2
     return sumNumber
 }
-let ans= addNumber(2,6)
+// let ans= addNumber(2,6)
+// console.log(ans);
+
+
+let ans=addNumber(addNumber(1,2),1)
 console.log(ans);
