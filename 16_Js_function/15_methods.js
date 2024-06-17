335b26ca737f5a3174bc2ece0ffa3bd38d07e49b
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-17 17:33:25 +0530

    Methods in javascript

---
+++ 16_Js_function/15_methods.js
@@ -0,0 +1,17 @@
+/*Methods:- action that can be performed on an object */
+
+const calculator = {
+  sum: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
+};
+
+console.log(calculator.sum(2, 3));
+console.log(calculator.sub(2, 3));
+console.log(calculator.mul(2, 3));
