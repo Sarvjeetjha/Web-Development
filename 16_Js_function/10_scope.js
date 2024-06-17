81b12a1a7daf18eb537bffb15f969f21cf2596d5
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-17 10:56:53 +0530

    global scope

--- 16_Js_function/10_scope.js
+++ 16_Js_function/10_scope.js
@@ -1,13 +1,31 @@
 //scope in js
 //function scope
-function calSum(num1,num2){
-    let sum =num1+num2
-    console.log(sum);
+// function calSum(num1,num2){
+//     let sum =num1+num2
+//     console.log(sum);
    
-}
-// console.log(sum);//we cant access sum variable outside function
-calSum(2,3);
+// }
+// // console.log(sum);//we cant access sum variable outside function
+// calSum(2,3);
 
 
 
 //global scope
+//  let sum=50 //global scope
+//  function calSum(a,b){
+    
+//     console.log(sum); //it is accessing outisde sum in case of function scope is not present
+
+
+//  }
+//  calSum(2,5)
+
+
+
+let sum=50 //global scope
+ function calSum(a,b){
+    let sum=a+b
+    console.log(sum); 
+
+ }
+ calSum(2,5)
