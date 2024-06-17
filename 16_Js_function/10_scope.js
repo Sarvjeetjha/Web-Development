673e38decdd1923e13a08912ee1b2e18b09a7787
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-17 10:51:31 +0530

    function scope of variable

---
+++ 16_Js_function/10_scope.js
@@ -0,0 +1,13 @@
+//scope in js
+//function scope
+function calSum(num1,num2){
+    let sum =num1+num2
+    console.log(sum);
+   
+}
+// console.log(sum);//we cant access sum variable outside function
+calSum(2,3);
+
+
+
+//global scope
