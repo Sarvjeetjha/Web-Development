a4e6681d88b7185b20db4d44f31394605c231b98
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-17 17:37:55 +0530

    Methods (shortHand)  in javascript

--- 16_Js_function/15_methods.js
+++ 16_Js_function/15_methods.js
@@ -1,17 +1,33 @@
 /*Methods:- action that can be performed on an object */
 
+// const calculator = {
+//   sum: function (a, b) {
+//     return a + b;
+//   },
+//   sub: function (a, b) {
+//     return a - b;
+//   },
+//   mul: function (a, b) {
+//     return a * b;
+//   },
+// };
+
+// console.log(calculator.sum(2, 3));
+// console.log(calculator.sub(2, 3));
+// console.log(calculator.mul(2, 3));
+
+//Methods shorthand()
 const calculator = {
-  sum: function (a, b) {
+  sum(a, b) {
     return a + b;
   },
-  sub: function (a, b) {
+  sub(a, b) {
     return a - b;
   },
-  mul: function (a, b) {
+  mul(a, b) {
     return a * b;
   },
 };
-
 console.log(calculator.sum(2, 3));
 console.log(calculator.sub(2, 3));
 console.log(calculator.mul(2, 3));
