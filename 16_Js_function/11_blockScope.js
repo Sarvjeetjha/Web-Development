289d81c6b9f2c61c7733da842ae259cb200177ce
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-17 11:20:30 +0530

    block scope

---
+++ 16_Js_function/11_blockScope.js
@@ -0,0 +1,34 @@
+//variable declared inside a {} blosk canot be accessed form outside the    block
+//it's applicable only for let and const not for var
+
+//********************************* */
+
+// {
+//     let a=5
+//     console.log(a);
+// }
+// console.log(a);//cannot accessed outside block
+//***************************** */
+
+
+// { 
+//     var a=5
+// }
+// console.log(a);
+//********************************* */
+
+
+// for(let i=1;i<=5;i++){
+//     console.log(i);//block scope
+// }
+// console.log(i); //cannot accessed outside scope
+
+
+let age=25
+if(age>18){
+    let str="adult"
+
+
+}
+// console.log(str); //cannot access outside scope
+
