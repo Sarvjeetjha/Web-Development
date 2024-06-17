faac0b6df569a5be719ae252d35fc125db113a30
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-17 17:02:12 +0530

    sum of number using function expresion

--- 16_Js_function/13_functionExpression.js
+++ 16_Js_function/13_functionExpression.js
@@ -1,7 +1,13 @@
 //function expression is used to store the reference of function definition in a variable
 
-const greet=function(){
-    console.log("hello")
-}
-greet( )
+// const greet=function(){
+//     console.log("hello")
+// }
+// greet( )
 
+
+
+const sumNumber=function(a,b){
+    return a+b
+}
+console.log(sumNumber(2,6));
